95e30ccb736d506f1ee1232d00a765e7460658b8
Ivan Sergeev <ahrejlok2007@ya.ru>
2021-08-20 12:23:11 +0500

    20 08 21 -3

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -2,25 +2,25 @@ import React from 'react'
 import classes from './ActiveQuiz.css'
 import AnswersList from '../AnswersList/AnswersList'
 
-const ActiveQuiz = props => (
-   
-    <div className={classes.ActiveQuiz}>
+const ActiveQuiz = props => {
+    return (
+      <div className={classes.ActiveQuiz}>
         <p className={classes.Question}>
-            <span>
-                <strong>2.</strong>&nbsp;
-               {props.question}
-            </span>
-          
-            <small>{props.answerNumber} из {props.quizLength}</small>
+        <span>
+          <strong>{props.answerNumber}.</strong>&nbsp;
+          {props.question}
+        </span>
+  
+          <small>{props.answerNumber} из { props.quizLength }</small>
         </p>
-
-    <AnswersList
-        state={props.state}
-        answers={props.answers}
-        onAnswerClick={props.onAnswerClick}
-    />
-
-    </div>
-)
-
-export default ActiveQuiz
+  
+        <AnswersList
+          state={props.state}
+          answers={props.answers}
+          onAnswerClick={props.onAnswerClick}
+        />
+      </div>
+    )
+  }
+  
+  export default ActiveQuiz
