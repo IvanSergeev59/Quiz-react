9f5ad2d2228956fa921e967dffe02b667ec82cf0
Ivan Sergeev <ahrejlok2007@ya.ru>
2021-09-29 12:11:36 +0500

    29 09 21 add reduc func create test

--- src/store/reducers/quiz.js
+++ src/store/reducers/quiz.js
@@ -1,4 +1,6 @@
-import {FETCH_QUIZ_SUCCESS, FETCH_QUIZES_ERROR, FETCH_QUIZES_START, FETCH_QUIZES_SUCCESS} from '../actions/actionTypes';
+import {FETCH_QUIZ_SUCCESS, QUIZ_NEXT_QUESTION, 
+  FINISH_QUIZ, QUIZ_SET_STATE,  FETCH_QUIZES_ERROR, QUIZ_RETRY,
+  FETCH_QUIZES_START, FETCH_QUIZES_SUCCESS} from '../actions/actionTypes';
 
 const initialState = {
   quizes: [],
@@ -29,6 +31,26 @@ export default function quizReducer(state = initialState, action) {
       return {
         ...state, loading: false, quiz: action.quiz
       }
+    case QUIZ_SET_STATE:
+      return {
+        ...state, answerState: action.answerState, results:action.results
+      }
+    case FINISH_QUIZ:
+      return {
+        ...state, isFinished: true
+      }
+      case  QUIZ_NEXT_QUESTION:
+        return {
+          ...state, answerState:null, activeQuestion: action.number
+        }
+      case QUIZ_RETRY:
+        return {
+          ...state, 
+          activeQuestion: 0,
+          answerState: null,
+          isFinished: false,
+          results: {}
+        }
     default:
       return state
   }
